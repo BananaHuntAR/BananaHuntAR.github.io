c016920511dc67f7772f7ec9198e1bfe6e7170d9
Marshall Zobel <dmzobel@gmail.com>
2018-05-16 15:46:20 -0400

    UPDATE: Marshall bio

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Image } from 'semantic-ui-react';
 import TeamMember from './TeamMember';
 import Marshall from '../img/Marshall.jpg';
 import Val from '../img/Val.jpeg';
 import Andrew from '../img/Andrew.png';
+import bananaTeam from '../img/bananaTeam.png';
 
 const Team = () => (
   <Grid columns={2} className="padding">
@@ -45,8 +46,7 @@ const Team = () => (
               City. He has prior work experience as a consultant for PwC and
               Grant Thornton. Marshall is graduate of Fullstack Academy of Code
               and the College of William and Mary. His interests include data
-              modeling and analysis, capital markets and financial services
-              (especially the 2008 financial crisis), and anything related to
+              modeling and analysis, capital markets, and anything related to
               film or baseball."
           imgUrl={Marshall}
           github="https://github.com/dmzobel"
